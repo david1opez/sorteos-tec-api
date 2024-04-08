89f7f6994401245dbb5cca0150b6f1e4226a0d6f
david1opez <degl0412@gmail.com>
2024-04-08 15:56:45 -0600

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 
 app.get('/datos', async (req, res) => {
     const db = await mysql.createConnection({
-        host: 'mysql-409f321-tec-beb1.a.aivencloud.com',
-        port: 21996,
-        user: 'andy',
-        password: 'AVNS_hF57l8rg5ij3KGUqJIb',
-        database: 'entrega1.4_2'
+        host: process.env.HOST,
+        port: parseInt(process.env.PORT!),
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
     });
     
     const data = await db.execute('SELECT * FROM compra')
